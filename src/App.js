f8b63277b6cb88ea8a2c659a4480dac5904e9f09
Lester Lucas Cerioli <lesterlucasit@hotmail.com>
2024-09-27 21:22:23 -0300

    Changing app text for English

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Gerador de Senha</h1>
+      <h1>Password Generator</h1>
         <PasswordForm />
       </header>
     </div>
